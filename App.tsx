9c9f1d6b5ffdd3e416d09a9786b857041fd8db40
Rafael Monroy <rafaelmonroy1990@gmail.com>
2023-05-17 22:30:34 -0700

    add sensor lib to detect user direction - WIP

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Dimensions, Platform } from 'react-native';
-import MapView, { Overlay } from 'react-native-maps';
-import Geolocation from 'react-native-geolocation-service';
+import { StyleSheet, View, Dimensions, Platform, Image } from 'react-native';
+import MapView, { Marker, Overlay, Region } from 'react-native-maps';
+import Geolocation, { GeoPosition } from 'react-native-geolocation-service';
 import { PERMISSIONS, request } from 'react-native-permissions';
+import { gyroscope } from 'react-native-sensors';
 
 const { width, height } = Dimensions.get('window');
+const arrowImage = require('./src/images/arrow.png');
+
+interface Location {
+  latitude: number;
+  longitude: number;
+}
 
 function App(): JSX.Element {
-  const [initialLocation, setInitialLocation] = useState({ latitude: 0, longitude: 0 });
+  const [initialLocation, setInitialLocation] = useState<Location>({ latitude: 0, longitude: 0 });
+  const [rotation, setRotation] = useState<number>(0);
+  const [gyroscopeSubscription, setGyroscopeSubscription] = useState<any>(null);
 
   useEffect(() => {
     requestLocationPermission();
+    startGyroscopeUpdates();
+    return () => {
+      stopGyroscopeUpdates();
+    };
   }, []);
 
   const requestLocationPermission = async () => {
@@ -28,34 +41,50 @@ function App(): JSX.Element {
 
   const getCurrentLocation = () => {
     Geolocation.getCurrentPosition(
-      position => {
+      (position: GeoPosition) => {
         const { latitude, longitude } = position.coords;
         setInitialLocation({ latitude, longitude });
       },
-      error => {
+      (error) => {
         console.warn('Error getting current location:', error);
       }
     );
   };
 
+  const startGyroscopeUpdates = () => {
+    const gyroscopeObservable = gyroscope.subscribe(({ x, y }) => {
+      const newRotation = Math.atan2(y, x) * (180 / Math.PI);
+      setRotation(newRotation);
+    });
+    setGyroscopeSubscription(gyroscopeObservable);
+  };
+
+  const stopGyroscopeUpdates = () => {
+    if (gyroscopeSubscription) {
+      gyroscopeSubscription.unsubscribe();
+      setGyroscopeSubscription(null);
+    }
+  };
+
+  const initialRegion: Region = {
+    latitude: 36.272100,
+    longitude: -115.011000,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121,
+  };
+
   return (
     <View style={styles.container}>
-
       <MapView
         style={styles.map}
         showsUserLocation
+        followsUserLocation
         userLocationPriority="high"
         mapType='mutedStandard'
         showsMyLocationButton
         userLocationCalloutEnabled
-        // followsUserLocation
         userInterfaceStyle='dark'
-        region={{
-          latitude: 36.272100,
-          longitude: -115.011000,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
+        initialRegion={initialRegion}
       >
         <Overlay
           image={require('./src/images/edc_map.png')}
@@ -64,8 +93,10 @@ function App(): JSX.Element {
             [36.277146, -115.004826],
           ]}
         />
+        <Marker coordinate={initialLocation} anchor={{ x: 0.5, y: 0.5 }} rotation={rotation}>
+          <Image source={arrowImage} style={[styles.arrowImage, { transform: [{ rotate: `${rotation}deg` }] }]} />
+        </Marker>
       </MapView>
-
     </View>
   );
 }
@@ -81,6 +112,10 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  arrowImage: {
+    width: 32,
+    height: 32,
+  }
 });
 
 export default App;
